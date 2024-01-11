3ec791e1ef1d4b608fb60ac433a074f5176b1e2b
Kakerlaken <woody.falcone.00@gmail.com>
2024-01-11 16:45:46 +0200

    make build

---
+++ index.e7b2b446.js
@@ -0,0 +1,3 @@
+document.getElementById("main-form").addEventListener("submit",function(e){e.preventDefault(),e.stopPropagation();for(var t=document.getElementsByClassName("main__form-input"),n=0;n<t.length;n++)t[n].value=""});//# sourceMappingURL=index.e7b2b446.js.map
+
+//# sourceMappingURL=index.e7b2b446.js.map
